df4d89e2fe77cf4ad88431f039466afb36c53e1a
Somsubhra Bairi <somsubhra.b@endurance.com>
2017-10-17 12:54:22 +0530

    initial commit

---
+++ src/main/java/org/structuredb/Application.java
@@ -0,0 +1,9 @@
+package org.structuredb;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Starting StructureDB");
+        System.out.println("Gracefully exiting StructureDB");
+    }
+}
